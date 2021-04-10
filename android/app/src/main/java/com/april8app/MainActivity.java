16a5d564a590d330ae0f24a8b39c9ff192f1087f
arefinncode <arefin.nordecode@gmail.com>
2021-04-10 17:16:13 +0600

    new commit ---why swipe is not working

--- android/app/src/main/java/com/april8app/MainActivity.java
+++ android/app/src/main/java/com/april8app/MainActivity.java
@@ -2,6 +2,11 @@ package com.april8app;
 
 import com.facebook.react.ReactActivity;
 
+
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+
 public class MainActivity extends ReactActivity {
 
   /**
@@ -12,4 +17,18 @@ public class MainActivity extends ReactActivity {
   protected String getMainComponentName() {
     return "April8App";
   }
+
+
+
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new ReactActivityDelegate(this, getMainComponentName()) {
+      @Override
+      protected ReactRootView createRootView() {
+       return new RNGestureHandlerEnabledRootView(MainActivity.this);
+      }
+    };
+  }
+
+
 }
